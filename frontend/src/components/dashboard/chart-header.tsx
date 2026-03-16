b0af12afefd0d2406392ef490c2fb0eda5704f69
Hamed Jimoh <hameed.jimoh1601@gmail.com>
2026-03-16 12:40:48 +0100

    Implements responsiveness for the ChartHeader component

--- frontend/src/components/dashboard/chart-header.tsx
+++ frontend/src/components/dashboard/chart-header.tsx
@@ -7,14 +7,14 @@ const tabs = ["Gradient ADK", "Managed KB", "Serverless Inference"];
 export default function ChartHeader() {
   const [currentTab, setCurrentTab] = useState(0);
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex flex-col-reverse gap-y-3 items-center justify-between xl:flex-row xl:gap-y-0">
       <ul className="flex gap-x-2 p-2 rounded-md bg-[#1c1d1f]">
         {tabs.map((tab, i) => (
           <li key={tab}>
             <Button
               variant="ghost"
               className={cn(
-                "px-5 py-2.5 text-sm font-medium border-b-2 transition-colors border-transparent text-muted-foreground hover:text-foreground",
+                "px-2 py-2.5 text-xs font-medium border-b-2 transition-colors border-transparent text-muted-foreground hover:text-foreground sm:px-5 sm:text-sm",
                 {
                   "bg-gray-600/30 text-gray-200": currentTab === i,
                 },
@@ -26,7 +26,7 @@ export default function ChartHeader() {
           </li>
         ))}
       </ul>
-      <span className="text-base text-muted-foreground">
+      <span className="text-sm text-muted-foreground sm:text-base">
         Powered by DigitalOcean Gradient AI
       </span>
     </div>
